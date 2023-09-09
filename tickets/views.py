48a0da2befcdc192186e56f8876a8e2cdf567479
A7madReda <95848196+A7madReda@users.noreply.github.com>
2023-09-09 18:21:16 +0300

    views without REST

--- tickets/views.py
+++ tickets/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.http.request import JsonResponse
 
 # Create your views here.
+
+# 1. without REST and no model query FBV -------> FUNCTION BASED VIEW
+def no_rest_no_model(request):
+    guests = [
+        {
+            'id':1,
+            'name':'omar',
+            'mobile':1021243072,
+            },
+        {
+            'id':2,
+            'name':'ali',
+            'mobile':124567895,
+        }
+    ]
+    
+    return JsonResponse (guests, safe=False)
